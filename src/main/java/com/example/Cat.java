0607189a81ec9d74784d68a11f886bc48b7682e6
Atya22 <aytaj2002eva@gmail.com>
2025-07-18 22:09:12 +0400

    Singleton design pattern thread not safe

--- src/main/java/com/example/Cat.java
+++ src/main/java/com/example/Cat.java
@@ -3,7 +3,7 @@ package com.example;
 public class Cat {
 
 
-    private final Logger logger = new Logger();
+    private final Logger logger = Logger.getINSTANCE();
 
     public void meow() {
         logger.log("Meow");
--- src/main/java/com/example/Logger.java
+++ src/main/java/com/example/Logger.java
@@ -1,8 +1,17 @@
 package com.example;
 
-public class Logger {
+public final class Logger {
+    private static Logger INSTANCE;
 
+    private Logger() {
+    }
 
+    public static Logger getINSTANCE(){
+    if(INSTANCE == null){
+        INSTANCE = new Logger();
+    }
+    return INSTANCE;
+    }
     void log(String logMessage) {
         System.out.println(logMessage);
     }
