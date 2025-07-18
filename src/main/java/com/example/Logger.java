98a9c946bd5de427c6a3b373de74bf7ff401208a
Atya22 <aytaj2002eva@gmail.com>
2025-07-18 22:12:46 +0400

    Singleton design pattern thread safe

--- src/main/java/com/example/Logger.java
+++ src/main/java/com/example/Logger.java
@@ -1,17 +1,12 @@
 package com.example;
 
-public final class Logger {
-    private static Logger INSTANCE;
+public enum Logger {
+    INSTANCE;
 
-    private Logger() {
+    public static Logger getINSTANCE() {
+        return INSTANCE;
     }
 
-    public static Logger getINSTANCE(){
-    if(INSTANCE == null){
-        INSTANCE = new Logger();
-    }
-    return INSTANCE;
-    }
     void log(String logMessage) {
         System.out.println(logMessage);
     }
