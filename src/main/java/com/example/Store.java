00df0e384c1755bd77631bbd8fbd13d4846e8573
Atya22 <aytaj2002eva@gmail.com>
2025-07-21 11:54:20 +0400

    The factory method desing pattern

--- src/main/java/com/example/Store.java
+++ src/main/java/com/example/Store.java
@@ -6,7 +6,7 @@ public class Store {
 
 
     public static void main(String[] args) {
-        buySnack("ChocolateBar");
+        buySnack("Chocolate Bar");
         buySnack("Chips");
         buySnack("Drink");
     }
--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -1,10 +1,26 @@
 package com.example;
 
-public class VendingMachine {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
-    Snack getSnack(String snackType) {
-        return null;
+public class VendingMachine {
+    private static final Map<String, Supplier<Snack>> snackMap = new HashMap<>();
+
+    static {
+        snackMap.put("chips", Chips::new);
+        snackMap.put("drink", Drink::new);
+        snackMap.put("chocolate bar", ChocolateBar::new);
+
     }
 
-
+    Snack getSnack(String snackType) {
+        var lowerCaseSnack = snackType.toLowerCase();
+        var snackSupplier = snackMap.get(lowerCaseSnack);
+        if (snackSupplier != null) {
+            return snackSupplier.get();
+        } else {
+            throw new IllegalArgumentException("Invalid snack type");
+        }
+    }
 }
