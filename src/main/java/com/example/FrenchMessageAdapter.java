774c15579d8609493b1ca1ef53493d024b674761
Atya22 <aytaj2002eva@gmail.com>
2025-07-24 00:31:41 +0400

    The adapter pattern

---
+++ src/main/java/com/example/FrenchMessageAdapter.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class FrenchMessageAdapter implements LocalizedMessage {
+
+    private final FrenchLocalizedMessage frenchLocalizedMessage;
+
+    public FrenchMessageAdapter() {
+        this.frenchLocalizedMessage = new FrenchLocalizedMessage();
+    }
+
+    @Override
+    public void sayHello() {
+        frenchLocalizedMessage.sayBonjour();
+    }
+}
