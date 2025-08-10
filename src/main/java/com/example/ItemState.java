ab75a20366de77b2fc6666d6453919c4a3e3fd92
Atya22 <aytaj2002eva@gmail.com>
2025-08-10 16:07:52 +0400

    The memento pattern

---
+++ src/main/java/com/example/ItemState.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class ItemState {
+    private int state;
+
+    public ItemState(int state) {
+        this.state = state;
+    }
+
+    public int getState() {
+        return this.state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
