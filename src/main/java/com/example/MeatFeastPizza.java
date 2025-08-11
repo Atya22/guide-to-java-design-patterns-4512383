c5a3a79723fb1807bb38d726decb829fd9d2a212
Atya22 <aytaj2002eva@gmail.com>
2025-08-11 14:08:37 +0400

    Template pattern

--- src/main/java/com/example/MeatFeastPizza.java
+++ src/main/java/com/example/MeatFeastPizza.java
@@ -1,18 +1,8 @@
 package com.example;
 
-public class MeatFeastPizza {
-
-    public void makeBase() {
-        System.out.println("Mix flour, yeast and salt.");
-        System.out.println("Roll out the dough.");
-    }
-
+public class MeatFeastPizza extends Pizza {
+    @Override
     public void addToppings() {
-        System.out.println("Add pepperoni, ham and chicken" );
+        System.out.println("Add pepperoni, ham and chicken");
     }
-
-    public void cook() {
-        System.out.println("Cook in the oven for 20 minutes");
-    }
-
 }
